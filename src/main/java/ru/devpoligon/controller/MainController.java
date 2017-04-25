fe4ce6f153c503e7cb51d16fbe5526de7ab8aadc
Ян Шопин <y_shopin@nposinergy.ru>
2017-04-25 22:35:17 +0300

    Initial project

---
+++ src/main/java/ru/devpoligon/controller/MainController.java
@@ -0,0 +1,17 @@
+package ru.devpoligon.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class MainController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView mainView(){
+        ModelAndView model = new ModelAndView();
+        model.setViewName("index");
+        return model;
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Java Trainer</title>
+</head>
+<body>
+<h1>Добро пожаловать на тест по JAVA</h1>
+</body>
+</html>
